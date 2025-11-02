3646c52fce6e51591b19bd4aff692efb5cb252ee
CoderSwarup03 <swarupmahata321@gmail.com>
2025-11-02 12:11:54 +0530

    verified

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -6,7 +6,7 @@ const TodoItem = ({ item, deleteTodo, toggleTodo }) => {
         <>
             <div className='bg-white rounded-md p-3 m-2'>
                 <div className='flex flex-col md:flex-row justify-between items-center border border-green-500 rounded-md p-2'>
-                    <div className='w-[90%] border-2 border-green-200 overflow-x-scroll'>
+                    <div className='w-[92%] overflow-x-scroll'>
                         <h1 className={`text-xl font-bold ${item.completed ? 'line-through' : ''}`}>{item.task}</h1>
                     </div>
                     <div className='flex justify-between gap-[60px] p-3'>
